000d2fab8dcbc335fac8ae7c9d59e0fefbf60494
Ismail Ba <ismailbak7@gmail.com>
2023-07-20 16:47:28 +0100

    Initialize The Project

---
+++ app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'to do app';
+}
